46539db18411a56287b81f2f328242397458033b
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 15:08:43 -0700

    rewrote paddings

--- components/RainbowHighlight.js
+++ components/RainbowHighlight.js
@@ -9,12 +9,12 @@ export const RainbowHighlight = ({ color, children }) => {
     <RoughNotation
       type="highlight"
       multiline={true}
-      padding = {[0,2]}
-      iterations = {1}
-      animationDuration = {animationDuration}
-      color = {color}
-      >
-        {children}
-      </RoughNotation>
+      padding={[0, 2]}
+      iterations={1}
+      animationDuration={animationDuration}
+      color={color}
+    >
+      {children}
+    </RoughNotation>
   );
 };
