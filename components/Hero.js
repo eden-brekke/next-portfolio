99d2db97697212eefeb18824927e90213e5f020a
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 15:09:15 -0700

    added tags n photo

--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import { RainbowHighlight } from "./RainbowHighlight";
+import Image from 'next/image';
+import Eden from '../assets/eden-medium-size.png'
 
 export default function Hero(){
-  const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
+  const colors = ["#CDB4DB", "#FFC8DD", "#FFAFCC", "#BDE0FE", "#A2D2FF"];
   return (
-    <div className = "flex flex-row flex-wrap justify-center items-start">
+    <div className = "flex flex-wrap justify-center items-start">
       {/* text container */}
       <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
         <RoughNotationGroup show ={true}>
@@ -37,12 +39,13 @@ export default function Hero(){
       <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
         <RoughNotationGroup show ={true}>
           <RainbowHighlight color={colors[4]}>
-            <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+            <h1 className ="text-4xl md:text-8xl font-bold text-gray-900 dark:text-gray-200 my-2">
               Craftswoman
             </h1>
           </RainbowHighlight>
         </RoughNotationGroup>
       </div>
+      <Image src={Eden} alt="Eden with a llama" width={400} height={700} className="" />
     </div>
   )
 }
