1198b22e920e84420a5ef07d6888c0e328741257
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 12:42:13 -0700

    got structure for project cards my own projects

---
+++ components/Projects.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Image from 'next/image'
+
+export default function Projects() {
+  return (
+    <section className="bg-gray-50 dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-gray-50 dark:bg-gray-800">
+        <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          Projects
+        </h1>
+      </div>
+      {/* start grid */}
+      <div className='bg-[#F1F1F1] dark:bg-gray-900'>
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40">
+          {/* card */}
+          <a href='https://tailwindmasterkit.com'
+          className='w-full block shadow-2xl'
+          >
+            <div className="relative overflow-hidden">
+              <Image
+                src='../assets/codle.png'
+                alt="codle"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
+                />
+                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                  Codle
+                </h1>
+                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                  01
+                </h1>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+    // mmore!
+  )
+}
