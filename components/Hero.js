c384229f7968f0b7412d769a0be352c600b3376c
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 14:03:13 -0700

    added more adjectives

--- components/Hero.js
+++ components/Hero.js
@@ -5,13 +5,40 @@ import { RainbowHighlight } from "./RainbowHighlight";
 export default function Hero(){
   const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
   return (
-    <div className = "flex flex-row justify-center items-start overflow-hidden">
+    <div className = "flex flex-row flex-wrap justify-center items-start">
       {/* text container */}
       <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
         <RoughNotationGroup show ={true}>
           <RainbowHighlight color={colors[0]}>
             <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
               Developer
+            </h1>
+          </RainbowHighlight>
+        </RoughNotationGroup>
+      </div>
+      <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+        <RoughNotationGroup show ={true}>
+          <RainbowHighlight color={colors[1]}>
+            <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              Scientist
+            </h1>
+          </RainbowHighlight>
+        </RoughNotationGroup>
+      </div>
+      <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+        <RoughNotationGroup show ={true}>
+          <RainbowHighlight color={colors[3]}>
+            <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              DataFiend
+            </h1>
+          </RainbowHighlight>
+        </RoughNotationGroup>
+      </div>
+      <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+        <RoughNotationGroup show ={true}>
+          <RainbowHighlight color={colors[4]}>
+            <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              Craftswoman
             </h1>
           </RainbowHighlight>
         </RoughNotationGroup>
