b465d807bcac3a4e310ffd4cb874e6b29d73aeeb
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 13:25:56 -0700

    added rendering for footer and nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
   <ThemeProvider defaultTheme = 'light' attribute= 'class'>
+    <Navbar />
     <Component {...pageProps} />
+    <Footer />
   </ThemeProvider>
   );
 }
