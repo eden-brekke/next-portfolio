831b3ef15e8e157cf76a3af83c0fc05849765672
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 15:09:00 -0700

    added links and cr

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,14 @@
+import Link from 'next/link'
 
 export default function Footer(){
   return(
-    <h1>Footer Placeholder</h1>
+    <>
+      <div className="flex justify-evenly h-1/6">
+      <p>Check out my socials: </p>
+      <Link href='http://github.com/eden-brekke'><a className='px-2 rounded-md py-1 text-gray-50 underline bg-teal-300 hover:bg-pink-400 hover:text-gray-900'>GitHub</a></Link>
+      <Link href='http://linkedin.com/eden-brekke'><a className='px-2 rounded-md py-1 text-gray-50 underline bg-teal-300 hover:bg-pink-400 hover:text-gray-900'>LinkedIn</a></Link>
+      <p>&copy; 2022 </p>
+      </div>
+    </>
   )
 }
