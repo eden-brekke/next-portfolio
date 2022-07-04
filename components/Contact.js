4be3ace53eb11b1366f64324719a71c7459f8f56
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 15:07:42 -0700

    working on css

--- components/Contact.js
+++ components/Contact.js
@@ -8,34 +8,34 @@ export default function Contact() {
           Contact
         </h1>
       </div>
-      <div className="relative z-10 rounded-md shadow-md bg-[#02044A] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
+      <div className="relative z-10 rounded-md shadow-md p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
         <div className = "grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className = "md:ml-4">
             <header className="">
               <h1 className="text-gray-50 font-semibold text-2xl">
                 Get in Touch, let&apos;s talk!
               </h1>
-              <p className="font-light text-base text-gray-200 mt-2">Fill in the details and I&apos;ll get back to you as soon as I can!</p>
+              <p className="font-light text-base mt-2">Fill in the details and I&apos;ll get back to you as soon as I can!</p>
             </header>
             <div className="icons-container inline-flex flex-col my-20">
               <div className = 'flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4'>
-                <p className='text-gray-50 font-light text-sm'>
-                  (425)-891-4115
+                <p className='font-light text-sm'>
+                  My Phone: (425)-891-4115
                 </p>
               </div>
               <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
-                <p className= "text-gray-50 font-light text-sm">contact@yourwebsite.com</p>
+                <p className= "font-light text-sm">My Email: eden.lorrai@gmail.com</p>
               </div>
               <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
             </div>
           </div>
         </div>
         <form classNam= "form rounded-lg bg-gray-50 p-4 flex flex-col">
-          <label htmlFor="name" className="text-sm text-gray-600 mx-4">{" "} Your Name</label>
+          <label htmlFor="name" className="text-sm mx-4">{" "} Your Name</label>
           <input type='text' className='font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500' name='name' />
-          <label htmlFor='email' className='text-sm text-gray-600 mx-4 mt-4'>Email</label>
+          <label htmlFor='email' className='text-sm mx-4 mt-4'>Email</label>
           <input type='text' className ='font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500' name='email'/>
-          <button type='submit' className='bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold'>
+          <button type='submit' className='bg-teal-300 text-gray-900 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold'>
             Send Message
           </button>
         </form>
