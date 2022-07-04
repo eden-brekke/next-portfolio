3baa0beea631b7b3eed3eb2e942f1cedcb1a0798
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 11:58:00 -0700

    added function and called containerblock, projects, experience, about functions

--- components/Homepage.js
+++ components/Homepage.js
@@ -2,3 +2,18 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import ContainerBlock from './ContainerBlock';
 import Projects from './Projects';
+import Experience from './Experience';
+import About from './About';
+
+export default function Home(){
+  return (
+    <ContainerBlock
+      title= "Eden Brekke - Developer, Scientist, Data Fiend"
+      description="Building a Portfolio"
+      >
+        <Projects />
+        <Experience />
+        <About />
+      </ContainerBlock>
+  );
+}
