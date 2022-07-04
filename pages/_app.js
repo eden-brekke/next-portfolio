d79cf3bed44e58a5fe37c303145801ef3844538e
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 14:02:12 -0700

    fix tailwind, render nav and footer on every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import '../styles/globals.css'
-import { ThemeProvider } from 'next-themes';
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
-  <ThemeProvider defaultTheme = 'light' attribute= 'class'>
-    <Navbar />
-    <Component {...pageProps} />
-    <Footer />
-  </ThemeProvider>
+    <ThemeProvider defaultTheme="light" attribute="class">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
