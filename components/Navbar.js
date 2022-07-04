7eebc95f9d68a561ff9974271f4fc3dfc0acb2de
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-04 12:04:53 -0700

    started function and set constants as well as a useeffect arrow function

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,3 +3,13 @@ import Link from "next/link";
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/router';
 
+export default function Navbar(){
+  const router = useRouter();
+  console.log(router.asPath)
+  const {theme, setTheme } = useTheme();
+  const [mounted, setMounted] =useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+}
