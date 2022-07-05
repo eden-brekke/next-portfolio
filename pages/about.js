5a02d0210efd03c1a4b80ca5db7951dd3b7e368d
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-05 11:11:04 -0700

    wrapped module in container block

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,11 @@
-import About from '../components/About';
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import About from "../components/About";
 
-export default function AboutPath() {
-  return(
-    <About />
-  )
+export default function about() {
+  return (
+    <ContainerBlock>
+      <About />
+    </ContainerBlock>
+  );
 }
