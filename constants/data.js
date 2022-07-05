c19babcc719c83cc836121706b39eec9fc9a6083
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-05 11:08:57 -0700

    added constant data about myself, more work to be done

---
+++ constants/data.js
@@ -0,0 +1,76 @@
+const userData = {
+  githubUsername: "eden-brekke",
+  name: "Eden Brekke",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/../assets/eden-medium-size.png",
+  email: "eden.lorrai@gmail.com",
+  phone: "(425)-891-4115",
+  address: "Seattle, WA",
+  projects: [
+    {
+      title: "Smells Like Devs Cooking",
+      link: "",
+      imgUrl: "",
+    },
+    {
+      title: "Tech Spotlight",
+      link: "",
+      imgUrl: "",
+    },
+    {
+      title: "CODLE",
+      link: "",
+      imgUrl: "",
+    },
+    {
+      title: "YourSpace",
+      link: "",
+      imgUrl: "",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves science and technology, and is excited about joining the two passions to push for advancements in medicine.",
+    description: [
+      ``,
+      ``,
+      `Write about the tech you've learned`,
+    ],
+    currentProject: "",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Student",
+      company: "Code Fellows",
+      year: "2022",
+      companyLink: "https://www.codefellows.org/",
+      desc: "Received a certificate for Full Stack Development with an emphasis in Python language.",
+    },
+    {
+      title: "Student",
+      company: "University of Washington",
+      year: "2019",
+      companyLink: "https://www.washington.edu/",
+      desc: "Received a Bachelor's degree in Molecular, Cellular, and Developmental Biology.",
+    },
+    {
+      title: "Tutor",
+      company: "Bellevue College",
+      year: "2015-2019",
+      companyLink: "https://www.bellevuecollege.edu/",
+      desc: "Worked with Students to help them learn and master mathematics, physics, biology, chemistry and more.",
+    },
+  ],
+  resumeUrl:
+    "https://docs.google.com/document/d/1XhYmuWSI8IvBFxiO4Q8uPOi8lDi-JoiEEuHtjT-C3Sc/edit?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/",
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/eden-brekke/",
+    github: "https://github.com/eden-brekke",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
