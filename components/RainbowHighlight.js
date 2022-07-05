7532614c2222074e551aabd6f4e79decdcd5c713
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-05 11:15:11 -0700

    small adjustments

--- components/RainbowHighlight.js
+++ components/RainbowHighlight.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { RoughNotation } from 'react-rough-notation';
+import React from "react";
+import { RoughNotation } from "react-rough-notation";
 
 export const RainbowHighlight = ({ color, children }) => {
-  // Change animation duration depending on length of text being animated 
-  const animationDuration = Math.floor( 30 * children.length);
+  // Change the animation duration depending on length of text we're animating (speed = distance / time)
+  const animationDuration = Math.floor(30 * children.length);
 
   return (
     <RoughNotation
