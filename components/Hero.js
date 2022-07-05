6ba95a0ca5273e9b5b33d943e1b9020eae0c5d96
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-07-05 11:14:04 -0700

    grabbed ref code and made adjustments, still need to style

--- components/Hero.js
+++ components/Hero.js
@@ -1,51 +1,115 @@
+// import React from "react";
+// import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+// import { RainbowHighlight } from "./RainbowHighlight";
+// import Image from 'next/image';
+// import Eden from '../assets/eden-medium-size.png'
+
+// export default function Hero(){
+//   const colors = ["#CDB4DB", "#FFC8DD", "#FFAFCC", "#BDE0FE", "#A2D2FF"];
+//   return (
+//     <div className = "flex flex-wrap justify-center items-start">
+//       {/* text container */}
+//       <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+//         <RoughNotationGroup show ={true}>
+//           <RainbowHighlight color={colors[0]}>
+//             <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+//               Developer
+//             </h1>
+//           </RainbowHighlight>
+//         </RoughNotationGroup>
+//       </div>
+//       <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+//         <RoughNotationGroup show ={true}>
+//           <RainbowHighlight color={colors[1]}>
+//             <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+//               Scientist
+//             </h1>
+//           </RainbowHighlight>
+//         </RoughNotationGroup>
+//       </div>
+//       <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+//         <RoughNotationGroup show ={true}>
+//           <RainbowHighlight color={colors[3]}>
+//             <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+//               DataFiend
+//             </h1>
+//           </RainbowHighlight>
+//         </RoughNotationGroup>
+//       </div>
+//       <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+//         <RoughNotationGroup show ={true}>
+//           <RainbowHighlight color={colors[4]}>
+//             <h1 className ="text-4xl md:text-8xl font-bold text-gray-900 dark:text-gray-200 my-2">
+//               Craftswoman
+//             </h1>
+//           </RainbowHighlight>
+//         </RoughNotationGroup>
+//       </div>
+//       <Image src={Eden} alt="Eden with a llama" width={400} height={700} className="" />
+//     </div>
+//   )
+// }
 import React from "react";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import { RainbowHighlight } from "./RainbowHighlight";
-import Image from 'next/image';
-import Eden from '../assets/eden-medium-size.png'
+import userData from "../constants/data";
+import Image from 'next/image'
+import avatarUrl from "../assets/eden-medium-size.png"
 
-export default function Hero(){
+export default function Hero() {
   const colors = ["#CDB4DB", "#FFC8DD", "#FFAFCC", "#BDE0FE", "#A2D2FF"];
   return (
-    <div className = "flex flex-wrap justify-center items-start">
-      {/* text container */}
-      <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
-        <RoughNotationGroup show ={true}>
+    <div className="flex flex-row justify-center items-start overflow-hidden">
+      {/* Text container */}
+
+      <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+        <RoughNotationGroup show={true}>
           <RainbowHighlight color={colors[0]}>
-            <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Developer
+            <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              Developer.
             </h1>
           </RainbowHighlight>
-        </RoughNotationGroup>
-      </div>
-      <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
-        <RoughNotationGroup show ={true}>
           <RainbowHighlight color={colors[1]}>
-            <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Scientist
+            <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              Scientist.
             </h1>
           </RainbowHighlight>
-        </RoughNotationGroup>
-      </div>
-      <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
-        <RoughNotationGroup show ={true}>
+          <RainbowHighlight color={colors[2]}>
+            <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              Data Fiend.
+            </h1>
+          </RainbowHighlight>
           <RainbowHighlight color={colors[3]}>
-            <h1 className ="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              DataFiend
+            <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              Craftswoman.
             </h1>
           </RainbowHighlight>
         </RoughNotationGroup>
       </div>
-      <div className = "w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
-        <RoughNotationGroup show ={true}>
-          <RainbowHighlight color={colors[4]}>
-            <h1 className ="text-4xl md:text-8xl font-bold text-gray-900 dark:text-gray-200 my-2">
-              Craftswoman
-            </h1>
-          </RainbowHighlight>
-        </RoughNotationGroup>
+      {/* Image container */}
+      <div className="hidden lg:block relative w-full md:w-1/2 -mr-40 mt-20">
+        <div className="w-3/4 ">
+          <Image src={avatarUrl} alt="avatar" width={400} height={600} className=" shadow" />
+          <div className="flex flex-row justify-between mt-4">
+            <div className="flex flex-row space-x-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-90deg-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z"
+                />
+              </svg>
+              <p className="font-mono">That&apos;s me</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <Image src={Eden} alt="Eden with a llama" width={400} height={700} className="" />
     </div>
-  )
+  );
 }
