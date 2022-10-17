789333a1e239d224f1b4491e3e4d0a27bf1bd40b
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-10-17 16:08:22 -0700

    add project

--- components/Projects.js
+++ components/Projects.js
@@ -4,18 +4,40 @@ import codle from '../assets/codlecloseup.png'
 import space from '../assets/yourspacehomepage.png'
 import devs from '../assets/devsLandingZoom.png'
 import tech from '../assets/techspotlight.png'
-import data from '../constants/data';
+import covid from '../assets/Screenshot.png'
 
 export default function Projects() {
   return (
-    <section className="bg-gray-50 dark:bg-gray-800">
-      <div className="max-w-6xl mx-auto h-48 bg-gray-50 dark:bg-gray-800">
-        <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left text-gray-50">
+    <section className="bg-gray-50 dark:bg-[#482d4a]">
+      <div className="max-w-6xl mx-auto h-48 bg-gray-50 dark:bg-[#482d4a]">
+        <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left text-gray-900 dark:text-gray-50">
           Projects
         </h1>
       </div>
       {/* start grid */}
-      <div className='bg-[#F1F1F1] dark:bg-gray-900'>        
+      <div className='bg-[#F1F1F1] dark:bg-[#2E1C2B]'>
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40">
+          {/* card */}
+          <a href='https://www.kaggle.com/code/edenbrekke/tech-spotlight-indeed-data-18may2022/notebook'
+          className='w-full block shadow-2xl'
+          >
+            <div className="relative overflow-hidden">
+              <Image
+                src={covid}
+                alt="covid19 data set"
+                width="900"
+                height="700"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
+                />
+                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                  Covid 19 Data Visualization
+                </h1>
+                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                  01
+                </h1>
+            </div>
+          </a>
+        </div>        
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40">
           {/* card */}
           <a href='https://www.kaggle.com/code/edenbrekke/tech-spotlight-indeed-data-18may2022/notebook'
@@ -33,7 +55,7 @@ export default function Projects() {
                   Tech Spotlight
                 </h1>
                 <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                  01
+                  02
                 </h1>
             </div>
           </a>
@@ -55,7 +77,7 @@ export default function Projects() {
                   Codle
                 </h1>
                 <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                  01
+                  03
                 </h1>
             </div>
           </a>
@@ -77,7 +99,7 @@ export default function Projects() {
                   Smells Like Devs Cooking
                 </h1>
                 <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                  01
+                  04
                 </h1>
             </div>
           </a>
@@ -99,63 +121,12 @@ export default function Projects() {
                   Your Space
                 </h1>
                 <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                  01
+                  05
                 </h1>
             </div>
           </a>
         </div>
       </div>
     </section>
-    // mmore!
   )
 }
-// import React from "react";
-// import userData from "../constants/data";
-
-// export default function Projects() {
-//   return (
-//     <section className="bg-white dark:bg-gray-800">
-//       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-//         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-//           Projects
-//         </h1>
-//       </div>
-//       {/* Grid starts here */}
-//       <div className="bg-[#F1F1F1] dark:bg-gray-900">
-//         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40">
-//           {userData.projects.map((proj, idx) => (
-//             <ProjectCard
-//               title={proj.title}
-//               link={proj.link}
-//               imgUrl={proj.imgUrl}
-//               number={`${idx + 1}`}
-//               key={idx}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// const ProjectCard = ({ title, link, imgUrl, number }) => {
-//   return (
-//     <a href={link} className="w-full block shadow-2xl">
-//       <div className="relative overflow-hidden">
-//         <div className="h-72 object-cover">
-//           <img
-//             src={imgUrl}
-//             alt="portfolio"
-//             className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
-//           />
-//         </div>
-//         <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-//           {title}
-//         </h1>
-//         <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-//           {number.length === 1 ? "0" + number : number}
-//         </h1>
-//       </div>
-//     </a>
-//   );
-// };
